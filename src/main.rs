e26a4792a11fa5a5156d8e1f4b6c6fd566e14fd0
Marian Beermann <public@enkore.de>
2022-06-09 00:06:42 +0200

    readimg

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use libc::{c_char, c_double, time_t, timeval};
 use memmap2::MmapRaw;
 use std::fs::OpenOptions;
-use std::io;
+use std::{io, slice};
 use std::mem::size_of;
 use std::thread::sleep;
 use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
@@ -27,7 +27,7 @@ struct MonitorSharedData {
     valid: u8,
     active: u8,
     signal: u8,
-    format: u8,
+    format: SubpixelOrder,
     imagesize: u32,
     last_frame_score: u32,
     audio_frequency: u32,
@@ -46,6 +46,28 @@ struct MonitorSharedData {
 }
 
 const _: [u8; 760] = [0; size_of::<MonitorSharedData>()];
+
+// zm_rgb.h
+
+#[derive(Copy, Clone, Debug)]
+#[repr(u32)]
+enum ColourType {
+    GRAY8 = 1,
+    RGB24 = 3,
+    RGB32 = 4,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(u8)]
+enum SubpixelOrder {
+    NONE = 2,  // grayscale
+    RGB = 6,
+    BGR = 5,
+    BGRA = 7,
+    RGBA = 8,
+    ABGR = 9,
+    ARGB = 10,
+}
 
 #[derive(Copy, Clone, Debug)]
 #[repr(u32)]
@@ -107,12 +129,14 @@ impl Monitor {
             let videostore_data = trigger_data.add(size_of::<MonitorTriggerData>());
             let shared_timestamps = videostore_data.add(size_of::<MonitorVideoStoreData>());
             let shared_images = shared_timestamps.add(image_buffer_count * size_of::<timeval>());
+            let image_alignment_adj = 64 - (shared_images as usize % 64);
+            let shared_images = shared_images.add(image_alignment_adj);
 
             let shared_data = shared_data as *const MonitorSharedData;
             let trigger_data = trigger_data as *const MonitorTriggerData;
             let videostore_data = videostore_data as *const MonitorVideoStoreData;
             let shared_timestamps = shared_timestamps as *const timeval;
-            let shared_images = shared_images.align_offset(64) as *const u8;
+            //let shared_images = shared_images as *const u8;
 
             assert_eq!((*shared_data).size, size_of::<MonitorSharedData>() as u32);
             assert_eq!((*trigger_data).size, size_of::<MonitorTriggerData>() as u32);
@@ -148,9 +172,27 @@ fn main() -> io::Result<()> {
     let monitor = Monitor::connect(mid)?;
     println!("Monitor shm valid: {}", monitor.valid());
 
+    let image_buffer_count = 3;  // needs to be retrieved from the database
+    let mut last_read_index = image_buffer_count;
+
+    let image_size = unsafe { (*monitor.shared_data).imagesize };
+
+    println!("Image size is claimed to be: {}", image_size);
+
     loop {
-        println!("Last write time: {:?}", monitor.last_write_time());
-        sleep(Duration::from_millis(500));
+        //println!("Last write time: {:?}", monitor.last_write_time());
+        //sleep(Duration::from_millis(500));
+
+        let last_write_index = unsafe { (*monitor.shared_data).last_write_index };
+        if last_write_index != last_read_index && last_write_index != image_buffer_count {
+            let timestamp = unsafe { *monitor.shared_timestamps.offset(last_write_index as isize) };
+            let timestamp = UNIX_EPOCH + Duration::from_secs(timestamp.tv_sec as u64) + Duration::from_micros(timestamp.tv_usec as u64);
+            println!("New image available at index {}, timestamp {:?}", last_write_index, timestamp);
+            last_read_index = last_write_index;
+
+            let image_data = unsafe { slice::from_raw_parts(monitor.shared_images, image_size as usize) };
+            let image_data = image_data.to_vec();
+        }
     }
     Ok(())
 }
