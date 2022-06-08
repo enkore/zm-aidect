587741ef7da549f9b1352082497d9ebbab32905d
Marian Beermann <public@enkore.de>
2022-06-08 21:39:56 +0200

    shared memory definitions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,130 @@
+use libc::{c_char, c_double, time_t, timeval};
+use memmap2::MmapRaw;
+use std::fs::OpenOptions;
+use std::io;
+use std::mem::size_of;
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+struct MonitorSharedData {
+    // size and offsets in ZM code is all wrong
+    size: u32,
+    last_write_index: i32,
+    last_read_index: i32,
+    state: u32,
+    capture_fps: c_double,
+    analysis_fps: c_double,
+    last_event_id: u64,
+    action: u32,
+    brightness: i32,
+    hue: i32,
+    colour: i32,
+    contrast: i32,
+    alarm_x: i32,
+    alarm_y: i32,
+    valid: u8,
+    active: u8,
+    signal: u8,
+    format: u8,
+    imagesize: u32,
+    last_frame_score: u32,
+    audio_frequency: u32,
+    audio_channels: u32,
+
+    startup_time: time_t,
+    zmc_heartbeat_time: time_t,
+    last_write_time: time_t,
+    last_read_time: time_t,
+
+    control_state: [u8; 256],
+
+    alarm_cause: [c_char; 256],
+    video_fifo_path: [c_char; 64],
+    audio_fifo_path: [c_char; 64],
+}
+
+const _: [u8; 760] = [0; size_of::<MonitorSharedData>()];
+
+#[derive(Copy, Clone, Debug)]
+#[repr(u32)]
+enum TriggerState {
+    TriggerCancel,
+    TriggerOn,
+    TriggerOff,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+struct MonitorTriggerData {
+    size: u32,
+    trigger_state: TriggerState,
+    trigger_score: u32,
+    padding: u32,
+    trigger_cause: [c_char; 32],
+    trigger_text: [c_char; 256],
+    trigger_showtext: [c_char; 256],
+}
+
+const _: [u8; 560] = [0; size_of::<MonitorTriggerData>()];
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+struct MonitorVideoStoreData {
+    // size in ZM is wrong
+    size: u32,
+    padding: u32, // padding here, not in ZM which is wrong
+    current_event: u64,
+    event_file: [c_char; 4096],
+    recording: timeval,
+}
+
+const _: [u8; 4128] = [0; size_of::<MonitorVideoStoreData>()];
+
+struct Monitor {
+    mmap: MmapRaw,
+
+    shared_data: *const MonitorSharedData,
+    trigger_data: *const MonitorTriggerData,
+    videostore_data: *const MonitorVideoStoreData,
+    shared_timestamps: *const timeval,
+    shared_images: *const u8,
+}
+
+impl Monitor {
+    fn connect(monitor_id: u32) -> io::Result<Monitor> {
+        let mmap_path = format!("/dev/shm/zm.mmap.{}", monitor_id);
+        let file = OpenOptions::new().read(true).write(true).open(&mmap_path)?;
+
+        let mmap = unsafe { MmapRaw::map_raw(&file)? };
+
+        let shared_data = mmap.as_ptr(); // as *const MonitorSharedData;
+
+        let image_buffer_count = 20;
+
+        unsafe {
+            let trigger_data = shared_data.add(size_of::<MonitorSharedData>());
+            let videostore_data = trigger_data.add(size_of::<MonitorTriggerData>());
+            let shared_timestamps = videostore_data.add(size_of::<MonitorVideoStoreData>());
+            let shared_images = shared_timestamps.add(image_buffer_count * size_of::<timeval>());
+
+            let shared_data = shared_data as *const MonitorSharedData;
+            let trigger_data = trigger_data as *const MonitorTriggerData;
+            let videostore_data = videostore_data as *const MonitorVideoStoreData;
+            let shared_timestamps = shared_timestamps as *const timeval;
+            let shared_images = shared_images as *const u8;
+
+            Ok(Monitor {
+                mmap,
+                shared_data,
+                trigger_data,
+                videostore_data,
+                shared_timestamps,
+                shared_images,
+            })
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mid = 5;
 }
