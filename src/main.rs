6c32881de430703f6104ad4421f40eec09c85f8e
Marian Beermann <public@enkore.de>
2022-06-09 13:01:03 +0200

    multirun

--- src/main.rs
+++ src/main.rs
@@ -190,26 +190,29 @@ fn main() -> TractResult<()> {
         // make the model runnable and fix its inputs and outputs
         .into_runnable()?;
 
-    let image = image::open("imago.png").unwrap().to_rgb8();
-    //let image = std::fs::read("imago")?;  // 1280x720 x4 (RGBA)
 
-    let resized = image::imageops::resize(&image, 224, 224,::image::imageops::FilterType::Triangle);
+    for file in vec!("19399-video-0001.png", "19399-video-0002.png", "19399-video-0003.png") {
+        let image = image::open(file).unwrap().to_rgb8();
+        //let image = std::fs::read("imago")?;  // 1280x720 x4 (RGBA)
 
-    let image: Tensor = tract_ndarray::Array4::from_shape_fn((1, 3, 224, 224), |(_, c, y, x)| {
-        let mean = [0.485, 0.456, 0.406][c];
-        let std = [0.229, 0.224, 0.225][c];
-        (resized[(x as _, y as _)][c] as f32 / 255.0 - mean) / std
-    }).into();
+        let resized = image::imageops::resize(&image, 224, 224, ::image::imageops::FilterType::Triangle);
 
-    let result = model.run(tvec!(image)).unwrap();
+        let image: Tensor = tract_ndarray::Array4::from_shape_fn((1, 3, 224, 224), |(_, c, y, x)| {
+            let mean = [0.485, 0.456, 0.406][c];
+            let std = [0.229, 0.224, 0.225][c];
+            (resized[(x as _, y as _)][c] as f32 / 255.0 - mean) / std
+        }).into();
 
-    let best = result[0]
-        .to_array_view::<f32>()?
-        .iter()
-        .cloned()
-        .zip(2..)
-        .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-    println!("result: {:?}", best);
+        let result = model.run(tvec!(image)).unwrap();
+
+        let best = result[0]
+            .to_array_view::<f32>()?
+            .iter()
+            .cloned()
+            .zip(2..)
+            .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+        println!("result: {:?}", best);
+    }
 
 
     /*let monitor = Monitor::connect(mid)?;
